f3504709b360497e944d3257ae9b31681ae55606
Pritam Roy <ratsdust4226@gmail.com>
2025-02-14 20:22:56 +0530

    changed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
+/// <reference types="vite/client" />
+
+
 import { useEffect } from "react";
 import { useSocketStore } from "./zustand/useSocketStore";
 import { io } from "socket.io-client";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard, { RoomWrapper } from "./pages/dashboard";
 import Login from './pages/login';
 import SignUp from './pages/signUp';
+
 
 function App() {
   const setSocket = useSocketStore((state) => state.setSocket);
@@ -25,10 +28,10 @@ function App() {
     return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/" element={<Dashboard />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/room/:orgId/*" element={<RoomWrapper />} />
+        {/* <Route path="/room/:orgId/*" element={<RoomWrapper />} /> */}
       </Routes>
     </Router>
   );
