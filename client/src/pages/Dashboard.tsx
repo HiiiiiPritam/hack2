c902d867ae4b3f9316f5f9d91c5794f4118699a1
Pritam Roy <ratsdust4226@gmail.com>
2025-02-14 21:14:30 +0530

    abhinav2

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import MapComponent from "@/components/MapComponent";
 import { useSocketStore } from "@/zustand/useSocketStore";
 import io from "socket.io-client";
 
